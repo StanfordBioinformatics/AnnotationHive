33f1078ab7ae25fc43fa238423fc4b5a795d617d
Amir Bahmani <abahman@ncsu.edu>
2017-11-08 09:43:40 -0800

    Google Inc.

--- src/main/java/com/google/cloud/genomics/cba/GGAnnotateVariants.java
+++ src/main/java/com/google/cloud/genomics/cba/GGAnnotateVariants.java
@@ -1,7 +1,7 @@
 package com.google.cloud.genomics.cba;
 
 /*
- * Copyright (C) 2016-2017 Stanford University.
+ * Copyright (C) 2016-2017 Google Inc. and Stanford University.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
@@ -12,6 +12,8 @@ package com.google.cloud.genomics.cba;
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
+ * The code was derived from https://github.com/googlegenomics/dataflow-java/blob/master/src/main/java/com/google/cloud/genomics/dataflow/pipelines/AnnotateVariants.java
+ *
  */
 
 
